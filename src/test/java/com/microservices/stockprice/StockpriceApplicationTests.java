caf7af7826cc9c458bc9da545da97fabb3e4337c
Bruno Dantas <brunoodantas7@gmail.com>
2021-11-03 22:39:15 -0300

    first project commit

---
+++ src/test/java/com/microservices/stockprice/StockpriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.stockprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockpriceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
